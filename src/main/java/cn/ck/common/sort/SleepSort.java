01a753b66b8b4b81cffa73d7460f580c98210a7b
wangyuming <ncuwym@qq.com>
2022-04-10 14:19:55 +0800

    init project.

---
+++ src/main/java/cn/ck/common/sort/SleepSort.java
@@ -0,0 +1,33 @@
+package cn.ck.common.sort;
+
+/**
+ * @author wangyuming
+ *
+ */
+public class SleepSort {
+
+    public static void main(String[] args) {
+        int[] nums = {1,5,67,89};
+        sleepSort(nums);
+    }
+
+    /**
+     * 睡眠排序
+     *
+     * @param nums
+     */
+    public static void sleepSort(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            final int num = nums[i];
+            new Thread (()->{
+                try {
+                    Thread.sleep(num);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(num);
+            }).start();
+        }
+
+    }
+}
